a9f9f2f627a87bead4fe7982291215fd913a0469
Ali11Mustafa <ali.00622274@gmail.com>
2022-03-02 23:20:32 +0300

    changes

---
+++ Longest Common Prefix/script.js
@@ -0,0 +1,11 @@
+
+let LongestCommonPrefix=(StrArr)=>{
+    if(StrArr.length===0)
+    return "the Array length is Zeros"
+
+
+}
+
+StrArr=["Ali","Ala","Amin"]
+
+console.log(LongestCommonPrefix=(StrArr));
